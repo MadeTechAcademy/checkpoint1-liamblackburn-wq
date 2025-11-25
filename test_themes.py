bc80f0037e0e218edd40eab7240e756294cadbad
Liam <liam.blackburn@madetech.com>
2025-11-25 10:53:11 +0000

    Initial test_themes_array implemented

--- test_themes.py
+++ test_themes.py
@@ -22,6 +22,18 @@ def test_html_contains_duties(tmp_path):
     assert "<li>" and "</li>" in content 
     assert "Duty" in content
 
+# as a user, i want to select the Bootcamp theme and see duties 1, 2, 3, 4 and 13 displayed in a separate html file
+
+def test_themes_array(tmp_path):
+    html_file = tmp_path / "bootcamp_duties.html"
+    theme_name = duties_list[0]["name"]
+    duties = duties_list[0]["duties"]
+    content = read_html(html_file)
+    assert theme_name in content
+    assert duties in content
+
+
 def testIt():
     assert len(duties_list) == 13
+
     
